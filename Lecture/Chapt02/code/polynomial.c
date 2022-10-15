3613a674c2a919abd4562305c604033b809e0c77
1chooo <hugo970217@gmail.com>
2022-10-15 23:03:46 +0800

    Add: Assignment, Lecture

---
+++ Lecture/Chapt02/code/polynomial.c
@@ -0,0 +1,24 @@
+#include "stdlib.h"
+#include "stdio.h"
+
+#define MAX_DEGREE 101
+#define MAX_TERM 100
+
+// typedef struct polynomial {
+//   int degree;
+//   float coef[MAX_DEGREE];
+// } Polynomial;
+
+typedef struct polynomial {
+  flaot coef;
+  int expon;
+} Polynomial;
+
+int main(void) {
+  Polynomial a;
+  int n;
+
+  a.degree = n;
+
+  return 0;
+}
